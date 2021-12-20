bc915e4538e193c01f82c12e99946513b860b2fe
Yaroslam <vestnik700@gmail.com>
2021-12-20 20:52:03 +0300

    some work

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
@@ -6,5 +5,5 @@ from django.views import generic
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('game')
     template_name = 'signup.html'
--- game/urls.py
+++ game/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('accounts.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('api/', include('api.urls')),
-    path('game/', include('game_app.urls')),
+    path('game/', include('game_app.urls'), name='game'),
 ]
